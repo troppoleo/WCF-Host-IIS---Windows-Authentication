5920e2f8d8863858572df4e0e54e5b4cdf13e5ed
troppoleo <troppoleo@gmail.com>
2016-02-21 10:41:19 +0100

    Primo Commit

---
+++ Client/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Client
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            localhost.Service1 myService = new localhost.Service1();
+            myService.Credentials = System.Net.CredentialCache.DefaultCredentials;
+            textBox1.Text = myService.GetData(123, true);
+            myService.Dispose();
+        }
+    }
+}
